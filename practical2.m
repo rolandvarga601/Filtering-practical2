8237ad191f66a8075523591a6d175c19a81cf75e
Roland <roland.varga601@gmail.com>
2021-01-01 22:25:33 +0100

    Experiments with Assignment 1 (EOD)
    EOD = End Of the Day. The step response and the noise seem to be sampling time dependent. Also the autosave files are ignored from this version.

--- practical2.m
+++ practical2.m
@@ -8,16 +8,22 @@
 %%% Input signal
 % We chose the following input signal because ...
 % The following plot shows the input signal , you can see that ...
-u = ones (10 ,1);
+% u = [zeros(20,1);10000*ones(80,1)];
+u = 10000*ones(10000,1);
 
-figure
+figure(1)
 plot (u,'.')
 xlabel ('Time (s)')
 ylabel ('Input ')
 title ('Input Signal ')
 %%% Sampling Frequency
 % We found the sampling frequency by ...
-fs = 1;
+fs1 = 1000;
+Ts1 = 1/fs1;
+fs2 = 100;
+Ts2 = 1/fs2;
+fs3 = 10;
+Ts3 = 1/fs3;
 %%% Other important design choises
 % We use the comments to provide clear motivations of all our design choices .
 % Everything we comment directly under this header will be published as
@@ -25,6 +31,28 @@ fs = 1;
 % The text under the following line will be published as a green comment .
 excitement_level = 100;
 % Excitement level
+
+STUDENTID = 5230543;
+y1 = exciteSystem(STUDENTID,10000*ones(5/Ts1,1),fs1);
+y2 = exciteSystem(STUDENTID,10000*ones(5/Ts2,1),fs2);
+y3 = exciteSystem(STUDENTID,10000*ones(5/Ts3,1),fs3);
+
+% Linearity check
+% u = 10000*ones(10/Ts1,1);
+% y1 = exciteSystem(STUDENTID,u,fs1);
+% y2 = exciteSystem(STUDENTID,2*u,fs1);
+
+y_data1 = timeseries(y1,linspace(0,(size(y1,1)-1)*Ts1,size(y1,1)));
+y_data2 = timeseries(y2,0:Ts2:(size(y2,1)-1)*Ts2);
+y_data3 = timeseries(y3,0:Ts3:(size(y3,1)-1)*Ts3);
+%y_data2 = timeseries(y2,0:1/(fs/2):99*(2/fs));
+figure(2)
+plot(y_data1,'.')
+hold on
+plot(y_data2,'x')
+plot(y_data3,'x')
+%ylim([-1500 1500])
+hold off
 %% Assignment 2: Identification
 %%% Model Estimation
 % We use the following identification method because ...
