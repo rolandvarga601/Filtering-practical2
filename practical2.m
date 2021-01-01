8d94597a1b7e3ea8170ad17bbb79f440bc73315f
Roland <roland.varga601@gmail.com>
2021-01-01 19:31:46 +0100

    Main MATLAB file init
    The main MATLAB code is initialized with the given piece of code

---
+++ practical2.m
@@ -0,0 +1,39 @@
+%% Filtering & Identification Practical Assignment 2
+% Jane Doe #123456
+% John Doe #654321
+
+% St. id.: 5230543
+
+%% Assignment 1
+%%% Input signal
+% We chose the following input signal because ...
+% The following plot shows the input signal , you can see that ...
+u = ones (10 ,1);
+
+figure
+plot (u,'.')
+xlabel ('Time (s)')
+ylabel ('Input ')
+title ('Input Signal ')
+%%% Sampling Frequency
+% We found the sampling frequency by ...
+fs = 1;
+%%% Other important design choises
+% We use the comments to provide clear motivations of all our design choices .
+% Everything we comment directly under this header will be published as
+% text .
+% The text under the following line will be published as a green comment .
+excitement_level = 100;
+% Excitement level
+%% Assignment 2: Identification
+%%% Model Estimation
+% We use the following identification method because ...
+% As you can see in our figure , we can expect that ...
+code = rand ; % important code
+
+%% Assignment 3: Validation
+%%% VAF
+morecode = 1+1;
+%%% Conlusions
+% after going through the id - cycle many times we found that by doing this
+% and that our results improved ....
